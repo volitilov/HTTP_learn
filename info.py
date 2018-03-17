e6387aa538cfb3d1f81234e87f0273fddddcf0ea
volitilov <volitilov@gmail.com>
2018-03-17 20:37:09 +0300

    add info: status code, http methods

---
+++ info.py
@@ -0,0 +1,70 @@
+HTTP ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+# HTTP -  протокол прикладного уровня передачи данных (изначально — 
+# в виде гипертекстовых документов в формате «HTML», в настоящий 
+# момент используется для передачи произвольных данных). Основой 
+# HTTP является технология «клиент-сервер», то есть предполагается 
+# существование:
+	Потребителей 
+	# (клиентов), которые инициируют соединение и посылают запрос;
+	Поставщиков 
+	# (серверов), которые ожидают соединения для получения запроса, 
+	# производят необходимые действия и возвращают обратно сообщение 
+	# с результатом.
+
+# документация на английском:
+# https://developer.mozilla.org/en-US/docs/Web/HTTP
+# https://en.wikipedia.org/wiki/Hypertext_Transfer_Protocol
+
+# документация на русском:
+# https://ru.wikipedia.org/wiki/HTTP
+
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+HTTP access control (CORS)
+#
+
+HTTP authentication
+#
+
+HTTP caching
+#
+
+HTTP compression
+#
+
+HTTP conditional requests
+#
+
+HTTP content negotiation
+#
+
+HTTP cookies
+#
+
+HTTP range requests
+#
+
+HTTP redirects
+#
+
+HTTP specifications
+#
+
+HTTP headers
+# смотреть docs/headers
+
+HTTP body
+# смотреть docs/body
+
+HTTP request methods
+# смотреть docs/request_methods
+
+HTTP response status codes
+# смотреть docs/status_code
+
+CSP directives
+#
+
+HTTP examples
+# смотреть docs/ex
